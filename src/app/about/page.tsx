065eac86f45de43af94cabd6fa43abea2f83be4e
Alexander <alexander.work.mailbox@gmail.com>
2023-06-17 21:40:16 +0600

    Changed about page style

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,24 +1,21 @@
 import { Metadata } from "next";
 import Link from "next/link";
 
-interface IProps {
-	copyright: string
-}
+import style from "../../styles/About/index.module.css";
 
-export default function About({copyright}: IProps) {
+export default function About() {
 
 	return (
 		<>
-			<h1>
+			<h1 className="title">
 				About us
 			</h1>
 			<p>
 				Non et, hic aperiam natus obcaecati, blanditiis maiores voluptates officia minus quibusdam dolore praesentium nam laboriosam earum provident reiciendis dicta explicabo vel itaque ut repellat iure, at, eos repudiandae! Autem. Molestiae sed, ea quam veniam. Totam deleniti voluptatibus quae veritatis officiis blanditiis, temporibus in vel! Minus nesciunt qui optio aut quis alias non beatae. Molestias voluptate expedita consequuntur nisi iusto. Tempora sed deserunt consequuntur, unde, id sequi ex illo sapiente at repellat laboriosam reiciendis voluptate maxime ratione cum in obcaecati inventore ad, odit neque fuga esse. Tenetur dolorem nam quia. Illum numquam facilis ipsum quidem dicta perferendis nulla adipisci omnis in quam accusamus, corrupti iusto, unde soluta eum laborum vero fuga quia. Cum quas numquam reiciendis, rem non in tempore? Aspernatur, laborum, repellendus? Officiis, quibusdam? Eius, perspiciatis officia recusandae pariatur reprehenderit delectus ipsa excepturi fugit minus, dolore dolorum amet sunt, iure aperiam autem explicabo sit odio nihil! Temporibus odit, debitis! Dicta, magnam, fugit. Amet ducimus suscipit fugit esse eum, earum, reprehenderit, dolor, placeat sequi illum facilis consectetur quaerat quasi neque rerum. Velit sequi culpa, nulla quasi labore nisi obcaecati ipsam. Pariatur itaque nemo mollitia officia, quaerat odio, ratione illum vero, eveniet sit incidunt facere atque saepe quos eum deleniti laudantium eaque earum veniam inventore minima repellat expedita voluptas. Quae, inventore. Nostrum suscipit praesentium dolores veniam ullam quasi harum, voluptatum possimus maxime!
 			</p>
-			<Link href="/about/authors">
+			<Link href="/about/authors" className={style.link}>
 				Our awesome team
 			</Link>
-			<br></br>
 		</>
 	)
 }
